b6a5b0fc2b899a24f744cdb5f74a67ed3e4824d6
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-08-11 00:56:14 +0100

    flickity carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+const Flickity = require("flickity");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -22,12 +23,42 @@ require("channels");
 
 // External imports
 import "bootstrap";
+import "flickity";
+import "flickity-imagesloaded";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import "./dropdown";
+// import "./slider";
 
 document.addEventListener("turbolinks:load", () => {
 	// Call your functions here, e.g:
 	// initSelect2();
+	const carousel = document.querySelector(".main-carousel");
+	const flkty = new Flickity(carousel, {
+		imagesLoaded: true,
+		accessibility: true,
+		// cellAlign: "center",
+		wrapAround: true,
+		pageDots: false,
+		resize: true,
+		percentPosition: false,
+	});
+	flkty.resize();
+
+	// const images = document.querySelectorAll(".carousel-cell img");
+	// const docStyle = document.documentElement.style;
+	// const transformProp =
+	// 	typeof docStyle.transform == "string" ? "transform" : "WebkitTransform";
+	// flkty.on("scroll", function () {
+	// 	flkty.slides.forEach(function (slide, i) {
+	// 		const img = images[i];
+	// 		const x = -(slide.target + flkty.x) / 3;
+	// 		img.style[transformProp] = "translateX( " + x + "px)";
+	// 	});
+	// });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	flkty.resize();
 });
