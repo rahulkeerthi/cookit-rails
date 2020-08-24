47a9c5806242e0817e72dfa0f9b8fc5bc985df27
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-08-24 18:53:17 +0100

    pages auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, except: [:favourites]
 
   def home
     @kits = Kit.order(created_at: :desc).page(params[:kit_page])
