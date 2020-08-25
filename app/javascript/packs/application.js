2c40c8735d47ed3a5d9f57d332abceeb417ac26d
Rahul Keerthi <rahulkeerthi2@gmail.com>
2020-08-25 10:44:48 +0100

    moved dropdown logic to app js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,21 +28,74 @@ import "flickity-imagesloaded";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import "./dropdown";
+// import { toggleController } from "./dropdown";
 
 document.addEventListener("turbolinks:load", () => {
+	// CAROUSELS
 	const carousel = document.querySelector(".main-carousel");
-	const flkty = new Flickity(carousel, {
-		imagesLoaded: true,
-		accessibility: true,
-		wrapAround: true,
-		pageDots: false,
-		resize: true,
-		percentPosition: false,
-	});
-	flkty.resize();
-});
+	if (carousel) {
+		const flkty = new Flickity(carousel, {
+			imagesLoaded: true,
+			accessibility: true,
+			wrapAround: true,
+			pageDots: false,
+			resize: true,
+			percentPosition: false,
+		});
+		flkty.resize();
+		window.addEventListener("resize", flkty.resize(), false);
+	}
 
-document.addEventListener("DOMContentLoaded", () => {
-	// flkty.resize();
+	// DROPDOWNS
+	const collapse = document.getElementsByClassName("navbar-collapse")[0];
+	const dropdowns = document.getElementsByClassName("dropdown");
+	const toggle = document.getElementsByClassName("navbar-toggler")[0];
+
+	// Toggle if navbar menu is open or closed
+	const toggleMenu = () => {
+		collapse.classList.toggle("collapse");
+		if (!collapse.classList.contains("collapse")) {
+			const avatar = document.getElementsByClassName(".nav-item.dropdown")[0];
+			avatar.classList.toggle("avatar-hide");
+			// collapse.classList.toggle("in");
+		}
+	};
+
+	// Close all dropdown menus
+	const closeMenus = () => {
+		for (let dropdown of dropdowns) {
+			dropdown.getElementsByClassName("dropdown-toggle")[0];
+			dropdown.classList.remove("dropdown-open");
+			dropdown.classList.remove("open");
+		}
+	};
+
+	// Add click handling to dropdowns
+	for (let dropdown of dropdowns) {
+		dropdown.addEventListener("click", function () {
+			if (document.body.clientWidth < 768) {
+				const open = this.classList.contains("open");
+				closeMenus();
+				if (!open) {
+					this.getElementsByClassName("dropdown-toggle")[0].classList.toggle(
+						"dropdown-open"
+					);
+					this.classList.toggle("open");
+				}
+			}
+		});
+	}
+
+	// Close dropdowns when screen becomes big enough to switch to open by hover
+	const closeMenusOnResize = () => {
+		if (document.body.clientWidth >= 768) {
+			closeMenus();
+			collapse.classList.add("collapse");
+			// collapse.classList.remove("in");
+		}
+	};
+
+	// Event listeners
+	window.addEventListener("resize", closeMenusOnResize, false);
+	toggle.addEventListener("click", toggleMenu, false);
 });
